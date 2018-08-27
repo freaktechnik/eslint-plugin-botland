24b7f0072cadc94185ae1a72c4a6691d867add71
Martin Giger <martin@humanoids.be>
2018-08-27 23:30:48 +0200

    fix: add some missing globals

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = {
         //"no-unreachable-code": require("./rules/no-unreachable-code"), no code in a block after a terminator
         //"arg-types": require("./rules/arg-types"), rundimentary arugments check (count and types)
         //"use-guarded-terminator": require("./rules/use-guarded-terminator"), terminators must have an if around them
+        //"no-naughty-array-creation"
         //"use-array-variables"
         "only-global-function-declaration": require("./rules/only-global-function-declaration"),
         "prefer-function": require("./rules/prefer-function"),
