be44ff42e5a5049bf7cc8a6772d929b91f00d1d0
Martin Giger <martin@humanoids.be>
2018-09-02 15:51:24 +0200

    tests: check types that are declared

--- test/api.js
+++ test/api.js
@@ -1,6 +1,18 @@
 import test from 'ava';
 import api from '../api.json';
 
+const KNOWN_TYPES = [
+        "x",
+        "y",
+        "entity",
+        "number",
+        "any",
+        "boolean",
+        "string"
+    ],
+    GLOBAL = "global:",
+    ENUM = "enum:";
+
 const hasAlias = (t, alias) => {
     const aliasedTo = api.aliases[alias];
     t.true(api.terminators.includes(aliasedTo) || api.functions.includes(aliasedTo));
@@ -15,7 +27,22 @@ const hasArgs = (t, func) => {
         t.true(Array.isArray(args));
         for(const arg of args) {
             t.is(typeof arg, "string");
-            //TODO check for expected arg types/formats.
+            let argType = arg;
+            if(argType.endsWith("...")) {
+                argType = argType.substr(0, argType.length - 3);
+            }
+            else if(argType.endsWith("[]")) {
+                argType = argType.substr(0, argType.length - 2);
+            }
+            if(argType.startsWith(GLOBAL)) {
+                t.true(argType.substr(GLOBAL.length) in api.globals);
+            }
+            else if(argType.startsWith(ENUM)) {
+                t.true(argType.substr(ENUM.length) in api.enums);
+            }
+            else {
+                t.true(KNOWN_TYPES.includes(argType));
+            }
         }
     }
 };
